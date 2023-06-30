41b622bfd8b0d08de9ae76e61ed112027fbfa1b7
Xenfroz <s.sacristan64@hotmail.com>
2023-06-30 17:15:02 +0200

    Ajout de la page Login (non fonctionnelle)

--- FrontEnd/travaux.js
+++ FrontEnd/travaux.js
@@ -16,11 +16,8 @@ async function createWorks() {
         figure.appendChild(titre);
     });
 
-    const boutonTous = document.querySelector(".btn-tous");
-
-    boutonTous.addEventListener("click", function () {
-        document.querySelector(".gallery").innerHTML = '';
-        travaux.forEach(travail => {
+    function affichageTravaux (projets) {
+        projets.forEach(travail => {
             const figure = document.createElement('figure');
             projetGalerie.appendChild(figure);
             const image = document.createElement('img');
@@ -30,81 +27,43 @@ async function createWorks() {
             titre.innerText = travail.title;
             figure.appendChild(titre);
         });
+    }
 
+    const boutonTous = document.querySelector(".btn-tous");
 
+    boutonTous.addEventListener("click", function () {
+        document.querySelector(".gallery").innerHTML = '';
+        affichageTravaux(travaux);
     });
 
-     const boutonObjet = document.querySelector(".btn-objet");
+    const boutonObjet = document.querySelector(".btn-objet");
 
-        boutonObjet.addEventListener("click", function () {
-            const travauxObjets = travaux.filter(function (travail) {
-                return travail.categoryId === 1;
-            });
-            document.querySelector(".gallery").innerHTML = '';
-            travauxObjets.forEach(travail => {
-                const figure = document.createElement('figure');
-                projetGalerie.appendChild(figure);
-                const image = document.createElement('img');
-                image.src = travail.imageUrl;
-                figure.appendChild(image);
-                const titre = document.createElement('figcaption');
-                titre.innerText = travail.title;
-                figure.appendChild(titre);
-            });
-
+    boutonObjet.addEventListener("click", function () {
+        const travauxObjets = travaux.filter(function (travail) {
+            return travail.categoryId === 1;
         });
+        document.querySelector(".gallery").innerHTML = '';
+        affichageTravaux(travauxObjets);
+    });
     
     const boutonAppartement = document.querySelector(".btn-appartement");
 
-        boutonAppartement.addEventListener("click", function () {
-            const travauxAppartement = travaux.filter(function (travail) {
-                return travail.categoryId === 2;
-            });
-            document.querySelector(".gallery").innerHTML = '';
-            travauxAppartement.forEach(travail => {
-                const figure = document.createElement('figure');
-                projetGalerie.appendChild(figure);
-                const image = document.createElement('img');
-                image.src = travail.imageUrl;
-                figure.appendChild(image);
-                const titre = document.createElement('figcaption');
-                titre.innerText = travail.title;
-                figure.appendChild(titre);
-            });
+    boutonAppartement.addEventListener("click", function () {
+        const travauxAppartement = travaux.filter(function (travail) {
+            return travail.categoryId === 2;
         });
+        document.querySelector(".gallery").innerHTML = '';
+        affichageTravaux(travauxAppartement);
+    });
 
     const boutonHotelRestaurant = document.querySelector(".btn-hotel-restaurant");
 
-        boutonHotelRestaurant.addEventListener("click", function () {
-            const travauxHotelRestaurant = travaux.filter(function (travail) {
-                return travail.categoryId === 3;
-            });
-            document.querySelector(".gallery").innerHTML = '';
-            travauxHotelRestaurant.forEach(travail => {
-                const figure = document.createElement('figure');
-                projetGalerie.appendChild(figure);
-                const image = document.createElement('img');
-                image.src = travail.imageUrl;
-                figure.appendChild(image);
-                const titre = document.createElement('figcaption');
-                titre.innerText = travail.title;
-                figure.appendChild(titre);
-            });
+    boutonHotelRestaurant.addEventListener("click", function () {
+        const travauxHotelRestaurant = travaux.filter(function (travail) {
+            return travail.categoryId === 3;
         });
+        document.querySelector(".gallery").innerHTML = '';
+        affichageTravaux(travauxHotelRestaurant);
+    });
 
 }
-
-// for (let i = 0; i < travaux.length; i++) {
-//     const projetGalerie = document.querySelector(".gallery");
-//     const projetElement = document.createElement("article");
-//     const imageElement = document.createElement("img");
-//     imageElement.src = travaux[i].image;
-// }
-// const idElement = document.createElement("p");
-// idElement.src = projet.id;
-// const titreElement = document.createElement("h2");
-// titreElement.innerText = projet.titre;
-// const imageElement = document.createElement("img");
-// imageElement.innerText = projet.image;
-// const categorieElement = document.createElement("p");
-// categorieElement.innerText = article.categorie;
